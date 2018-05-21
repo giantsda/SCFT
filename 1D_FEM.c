59bddb89c728f9372e1c2ae08b24208d4b3de1f9
chenshen <chenshen@rams.colostate.edu>
2018-05-21 07:55:06 -0400

    add andmix.c, I am going to make simple_FEM_1D_transient(xin,xout) in order to use andmix It is vetter to sve before that!

--- 1D_FEM.c
+++ 1D_FEM.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 #include <petscksp.h>
+#include <petscdm.h>
+#include <petscdmda.h>
+#include <petscsnes.h>
+#include <stdlib.h>
+#include <math.h>
+#include "nr.h"
+#include "nrutil.h"
+
+void
+myfun (int n, double *x, double *xnew);
+double*
+adm (double *x, int n, int *check, void
+(*myfun) (int, double *x, double *xnew),
+     int flag);
 
 double **
 Matcreate (int r, int c)
@@ -19,8 +33,10 @@ Matfree (double** A, int r, int c)
 }
 
 int
-main ()
+simple_FEM_1D_transient (int N, double* yita_middle, double * f0,
+			 double* f0_given)
 {
+  // N is the total number of nodes in 1D problem.
   KSP ksp;
   PC pc;
   Mat A, B, C, D;
@@ -32,28 +48,35 @@ main ()
   if (ierr)
     return ierr;
   comm = MPI_COMM_SELF;
-  int N = 5; // N is the total number of nodes in 1D problem.
+
   double L = 1., dt = 0.01, t = 0, h = L / (N - 1); // L is the length of the space domain
-  double solution[N];
+  double solution[N], yita[N];
+  int de;
+  // add two fixed point of yita
+  yita[0] = 0.;
+  yita[N] = 0.;
+  for (int i = 1; i < N - 1; i++)
+    {
+      yita[i] = yita_middle[i - 1];
+    }
+//  for (int i = 0; i < N; i++)
+//    printf ("%f\n", yita[i]);
+//  scanf ("%d", &de);
+
   int time_step = 10;
-  double yita[N];
+
   PetscInt ix[N];
   double** solution_store = Matcreate (N + 1, time_step + 1);
-  for (int i = 0; i < N; i++)
-    {
-      yita[i] = 1.;
-    }
-
   ierr = MatCreateSeqAIJ (comm, N, N, 3, NULL, &A);
   ierr = MatCreateSeqAIJ (comm, N, N, 3, NULL, &B);
   ierr = MatCreateSeqAIJ (comm, N, N, 3, NULL, &C);
-  CHKERRQ (ierr);
+  CHKERRQ(ierr);
   ierr = VecCreateSeq (comm, N, &b);
-  CHKERRQ (ierr);
+  CHKERRQ(ierr);
   ierr = VecDuplicate (b, &X);
-  CHKERRQ (ierr);
+  CHKERRQ(ierr);
   ierr = VecDuplicate (b, &xold);
-  CHKERRQ (ierr);
+  CHKERRQ(ierr);
 
   PetscScalar vforA[3] =
     { h / 6, 2. / 3 * h, h / 6 };
@@ -95,28 +118,28 @@ main ()
   for (PetscInt i = 0; i < N; i++)
     ix[i] = i;
   ierr = VecGetValues (xold, N, ix, solution);
-  CHKERRQ (ierr);
+  CHKERRQ(ierr);
 
   solution_store[0][0] = 0;
   for (int i = 1; i < N; i++)
     solution_store[i + 1][0] = solution[i];
 
   ierr = MatAssemblyBegin (A, MAT_FINAL_ASSEMBLY);
-  CHKERRQ (ierr);
+  CHKERRQ(ierr);
   ierr = MatAssemblyEnd (A, MAT_FINAL_ASSEMBLY);
-  CHKERRQ (ierr);
+  CHKERRQ(ierr);
   ierr = MatAssemblyBegin (B, MAT_FINAL_ASSEMBLY);
-  CHKERRQ (ierr);
+  CHKERRQ(ierr);
   ierr = MatAssemblyEnd (B, MAT_FINAL_ASSEMBLY);
-  CHKERRQ (ierr);
+  CHKERRQ(ierr);
   ierr = MatAssemblyBegin (C, MAT_FINAL_ASSEMBLY);
-  CHKERRQ (ierr);
+  CHKERRQ(ierr);
   ierr = MatAssemblyEnd (C, MAT_FINAL_ASSEMBLY);
-  CHKERRQ (ierr);
+  CHKERRQ(ierr);
   ierr = VecAssemblyBegin (xold);
-  CHKERRQ (ierr);
+  CHKERRQ(ierr);
   ierr = VecAssemblyEnd (xold);
-  CHKERRQ (ierr);
+  CHKERRQ(ierr);
   MatDuplicate (A, MAT_COPY_VALUES, &D);
 
 //  printf ("A:\n");
@@ -135,7 +158,6 @@ main ()
 //  ierr = VecView (xold, 0);
 //  CHKERRQ (ierr);
 
-
   // TODO: change this to a better time marching method.
 
   ierr = MatAXPY (D, dt, B, SAME_NONZERO_PATTERN);  // A=A+dt*B
@@ -145,29 +167,29 @@ main ()
   ierr = MatSetValue (D, N - 1, N - 1 - 1, 0.0, INSERT_VALUES);
   ierr = MatSetValue (D, N - 1, N - 1, 1.0, INSERT_VALUES);
   ierr = MatAssemblyBegin (D, MAT_FINAL_ASSEMBLY);
-  CHKERRQ (ierr);
+  CHKERRQ(ierr);
   ierr = MatAssemblyEnd (D, MAT_FINAL_ASSEMBLY);
-  CHKERRQ (ierr);
+  CHKERRQ(ierr);
 
 //  printf ("D:\n");
 //  ierr = MatView (D, PETSC_VIEWER_STDOUT_WORLD);
 
   ierr = KSPCreate (comm, &ksp);
-  CHKERRQ (ierr);
+  CHKERRQ(ierr);
   ierr = KSPSetOperators (ksp, D, D);
-  CHKERRQ (ierr);
+  CHKERRQ(ierr);
   ierr = KSPSetType (ksp, KSPCG);
-  CHKERRQ (ierr);
+  CHKERRQ(ierr);
   ierr = KSPSetInitialGuessNonzero (ksp, PETSC_TRUE);
-  CHKERRQ (ierr);
+  CHKERRQ(ierr);
   ierr = KSPGetPC (ksp, &pc);
-  CHKERRQ (ierr);
+  CHKERRQ(ierr);
   ierr = PCSetType (pc, PCICC);
-  CHKERRQ (ierr);
+  CHKERRQ(ierr);
   ierr = KSPSetFromOptions (ksp);
-  CHKERRQ (ierr);
+  CHKERRQ(ierr);
   ierr = KSPSetUp (ksp);
-  CHKERRQ (ierr);
+  CHKERRQ(ierr);
 
   for (int i = 1; i < time_step + 1; i++)
     {
@@ -179,9 +201,9 @@ main ()
       ierr = KSPSolve (ksp, b, X);
       VecAssemblyBegin (X);
       VecAssemblyEnd (X);
-      CHKERRQ (ierr);
+      CHKERRQ(ierr);
       ierr = VecGetValues (X, N, ix, solution);
-      CHKERRQ (ierr);
+      CHKERRQ(ierr);
       // store X to solution_store
       for (int j = 0; j < N; j++)
 	{
@@ -192,21 +214,21 @@ main ()
     }
 
   ierr = KSPDestroy (&ksp);
-  CHKERRQ (ierr);
+  CHKERRQ(ierr);
   ierr = MatDestroy (&A);
-  CHKERRQ (ierr);
+  CHKERRQ(ierr);
   ierr = MatDestroy (&B);
-  CHKERRQ (ierr);
+  CHKERRQ(ierr);
   ierr = MatDestroy (&C);
-  CHKERRQ (ierr);
+  CHKERRQ(ierr);
   ierr = MatDestroy (&D);
-  CHKERRQ (ierr);
+  CHKERRQ(ierr);
   ierr = VecDestroy (&b);
-  CHKERRQ (ierr);
+  CHKERRQ(ierr);
   ierr = VecDestroy (&X);
-  CHKERRQ (ierr);
+  CHKERRQ(ierr);
   ierr = VecDestroy (&xold);
-  CHKERRQ (ierr);
+  CHKERRQ(ierr);
 
 //   Plot results
   printf ("solution_store: \n");
@@ -219,7 +241,7 @@ main ()
 
 //   integrate for f0
 //   TODO:change this to better integration method
-  double f0[N];
+
   printf ("f0: \n");
   for (int i = 0; i < N; i++)
     {
@@ -234,8 +256,65 @@ main ()
 	}
       printf ("%f \n", f0[i]);
     }
-
   ierr = PetscFinalize ();
   printf ("have a nice day! \n");
   return ierr;
 }
+
+int
+main ()
+{
+  printf ("Hello world!\n");
+  int N = 5;
+  double yita_middle[N - 2]; // initial guess, the ends are bounded
+  for (int i = 0; i < N - 2; i++)
+    {
+      yita_middle[i] = i + 1;
+    }
+  double f0[N]; // results
+  // calculate f0_given so that I can use andmix
+  double tau = 0.2;
+  double f0_given[N];
+  double x[N];
+  for (int i = 0; i < N; i++)
+    {
+      f0_given[i] = 1.;
+      x[i] = i * 1. / (N - 1);
+    }
+  int x_left = ceil (N * tau / 1.);
+  for (int i = 0; i < x_left; i++)
+    {
+      f0_given[i] = pow ((exp (4 * tau * x[i] / (tau * tau - x[i] * x[i])) - 1),
+			 2)
+	  / pow (((exp (4 * tau * x[i] / (tau * tau - x[i] * x[i])) + 1)), 2);
+      f0_given[N - i - 1] = f0_given[i];
+    }
+//  for (int i = 0; i < N; i++)
+//    {
+//      printf (">>%f \n ", f0_given[i]);
+//    }
+  simple_FEM_1D_transient (N, yita_middle, f0, f0_given);
+
+
+
+
+
+
+
+//  double in[2] =
+//    { 0, 0 };
+//  int check = 1;
+//  double* haha = adm (in, 2, &check, myfun, 0);
+//  for (int i = 0; i < 2; i++)
+//    printf ("%f \n", haha[i]);
+//  return 100;
+}
+
+void
+myfun (int n, double * x, double * xnew)
+{
+  xnew[0] = x[1] * 0.5 - 2;
+  xnew[1] = x[0] + 3;
+  printf ("x=%.14f, xnew=%.14f \n", *x, *xnew);
+}
+
--- Makefile
+++ Makefile
@@ -1,12 +1,21 @@
-1D_FEM.o : 1D_FEM.c  
-	mpicc 1D_FEM.c -I /home/chen/Desktop/software/petsc/petsc-3.5.4/include -I /home/chen/Desktop/software/petsc/petsc-3.5.4/x86_64/include -I /usr/lib/openmpi/include -L /home/chen/Desktop/software/petsc/petsc-3.5.4/x86_64/lib/  -Wl,-rpath,/home/chen/Desktop/software/petsc/petsc-3.5.4/x86_64/lib  -lpetsc -g
+nrutil.o: nrutil.c
+	gcc nrutil.c -c -g
 
-all: 1D_FEM.c 
-	mpicc 1D_FEM.c -Wall -I /home/chen/Desktop/software/petsc/petsc-3.5.4/include -I /home/chen/Desktop/software/petsc/petsc-3.5.4/x86_64/include -I /usr/lib/openmpi/include -L /home/chen/Desktop/software/petsc/petsc-3.5.4/x86_64/lib/  -Wl,-rpath,/home/chen/Desktop/software/petsc/petsc-3.5.4/x86_64/lib  -lpetsc -g 
+gaussj.o: gaussj.c  
+	gcc gaussj.c  -c -g  
+
+adm.o: adm.c gaussj.o  
+	gcc adm.c gaussj.o  -c -lm -g
+
+1D_FEM.o: 1D_FEM.c   
+	gcc 1D_FEM.c -c  -I /home/chen/Desktop/software/petsc/petsc-3.5.4/include -I /home/chen/Desktop/software/petsc/petsc-3.5.4/x86_64/include -I /usr/lib/openmpi/include -g
+
+all: 1D_FEM.o adm.o gaussj.o nrutil.o
+	mpicc 1D_FEM.o adm.o gaussj.o nrutil.o -o a.out -Wall -I /home/chen/Desktop/software/petsc/petsc-3.5.4/include -I /home/chen/Desktop/software/petsc/petsc-3.5.4/x86_64/include -I /usr/lib/openmpi/include -L /home/chen/Desktop/software/petsc/petsc-3.5.4/x86_64/lib/  -Wl,-rpath,/home/chen/Desktop/software/petsc/petsc-3.5.4/x86_64/lib  -lpetsc -g -lm
 
 
-
-run:
+run: 1D_FEM.o adm.o gaussj.o nrutil.o
+	mpicc 1D_FEM.o adm.o gaussj.o nrutil.o -o a.out -Wall -I /home/chen/Desktop/software/petsc/petsc-3.5.4/include -I /home/chen/Desktop/software/petsc/petsc-3.5.4/x86_64/include -I /usr/lib/openmpi/include -L /home/chen/Desktop/software/petsc/petsc-3.5.4/x86_64/lib/  -Wl,-rpath,/home/chen/Desktop/software/petsc/petsc-3.5.4/x86_64/lib  -lpetsc -g -lm   
 	./a.out
 	 
 
